4bb809aa4478b35c0f7af341fcd995064497e507
mpancia <msp@themsp.org>
2016-05-11 19:01:58 -0700

    Removed unnecessary functions.

--- R/utils.R
+++ R/utils.R
@@ -1,22 +1,5 @@
 restBaseURL <- "https://rxnav.nlm.nih.gov/REST/"
 
-# Exhaust the search result over all pages.
-exhaust_search <- function(FUN = searchFunction, PARSER = parseFunction, ...) {
-  results <- list()
-  curPage <- 1
-  keepSearching <- TRUE
-  while (keepSearching == TRUE) {
-    curResult <- PARSER(FUN(..., pageNumber = curPage))
-    if (!is.null(curResult)) {
-      results <- c(results, list(curResult))
-      curPage <- curPage + 1
-    } else {
-      keepSearching <- FALSE
-    }
-  }
-  unlist(results, recursive = F)
-}
-
 # Generic parser function for various response types.
 parse_results <- function(result) {
   if(status_code(result) != 200){
